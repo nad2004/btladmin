1866590635bd29cdec36bc4125a29670d784fbb7
nad2004 <91653501+nad2004@users.noreply.github.com>
2024-09-20 23:04:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: Number(process.env.PORT) || 3000,
   },
   resolve: {
